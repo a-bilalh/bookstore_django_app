5676bf3e4d5d4af90729252c9e026c888a7907f0
a-bilalh <abilalh7@gmail.com>
2025-09-11 23:44:39 -0700

    The api between django and react was created and it works successfully

--- backend/bookStoreApp/views.py
+++ backend/bookStoreApp/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-from models import Book
+from rest_framework import  viewsets
+from .models import Book
+from .serializers import BookSerializer
+
 
 # Create your views here.
-def book_list(request):
-    books = Book.objects.all().values()
-    return JsonResponse(list(books), safe=False)
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
