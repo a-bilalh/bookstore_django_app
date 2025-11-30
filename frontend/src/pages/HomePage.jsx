f9ec5d121f5a37a0fcfa3ba1e4e7b62f405a9e90
Ahmad <abilalh7@gmail.com>
2025-11-29 21:29:06 -0800

    conditional rendering was added for registrationForm in the homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,21 @@
 
-
 export default function Home() {
+
+    const[showRegistrationForm, setShowRegistrationForm] = useState(false);
+    const[showLoginForm, setShowLoginForm] = useState(false);
+
+
+
         
     return (
         <>
             <Header />
             <MyHorizontalList category={"fiction"} count={10}/>
             <MyHorizontalList category={"nonfiction"} count={10}/>
+
+            {showRegistrationForm && <RegistrationForm
+                        closeRegister={() => setShowRegistrationForm(false)} LoginForm={LoginForm} />}
+            {showLoginForm && <LoginForm closeLogin={() => setShowLoginForm(false)} />}
         </>
     )
 
