2ba96354e7cdee85adf994f1839b99a3978f56c6
Ahmad <abilalh7@gmail.com>
2025-12-29 22:38:47 -0600

    created utils/csv_helpers.py and inside it |export_book_fields_as_csv| and |import_books_from_csv| functions

---
+++ backend/backend/utils/csv_helpers.py
@@ -0,0 +1,37 @@
+import csv
+
+
+
+"""
+This functions takes the given book fields and export the coresponding values as a CSV file.
+"""
+def export_book_fields_as_csv( book_fields, file_path ):
+
+    book_values = Book.objects.values(*book_fields)
+    with open(file_path, mode='w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=book_fields)
+        writer.writeheader()
+        writer.writerows(book_values)
+
+
+"""
+This function imports book descriptions from a CSV file and updates the corresponding Book entries in the database.
+"""
+def import_books_from_csv(file_path):
+
+    books_ids_descriptions = {}
+
+    # Read CSV file and populate the dictionary
+    with open(file_path, mode='r', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        for row in reader:
+            books_ids_descriptions[ row['id'] ] = row['description']
+
+    # Update Book entries in the database
+    for id, description in books_ids_descriptions.items():
+    
+        row = Book.objects.filter(id=id).update(description=description)
+        if row == 0:
+            print(f"No book found with id: {id}")
+    
